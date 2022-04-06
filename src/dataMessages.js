371e2772230e655b596821e7c3d267bcea8983d1
valentinsld <valentin.salaud@gmail.com>
2022-04-06 22:42:29 +0200

    feat(dataMessage) parse data to get delay

--- src/dataMessages.js
+++ src/dataMessages.js
@@ -1,11 +1,26 @@
 var { default: srtParser2 } = require('srt-parser-2')
 const fs = require('fs');
+const { convertTimeToMilis } = require('./helpers/convertTimeToMilis');
+
+const changeTimeData = (data) => {
+  let lastTime = 0
+  data.map((d) => {
+    d.startTimeMilis = convertTimeToMilis(d.startTime)
+    d.endTimeMilis = convertTimeToMilis(d.endTime)
+    d.delayLastMilis = d.startTimeMilis  - lastTime
+    lastTime = d.startTimeMilis
+
+    return d
+  })
+
+  return data
+}
 
 var parser = new srtParser2()
 const contentIntro = fs.readFileSync('./src/assets/Intro.srt').toString();
 const contentOutro = fs.readFileSync('./src/assets/Intro.srt').toString();
 
-var dataIntro = parser.fromSrt(contentIntro);
-var dataOutro = parser.fromSrt(contentOutro);
+var dataIntro = changeTimeData(parser.fromSrt(contentIntro));
+var dataOutro = changeTimeData(parser.fromSrt(contentOutro));
 
 module.exports = { dataIntro, dataOutro }
