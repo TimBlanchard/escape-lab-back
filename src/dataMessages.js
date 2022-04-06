e8560d6bbf4ac2bd743a2039a723ab4e03b1863a
valentinsld <valentin.salaud@gmail.com>
2022-04-06 22:48:21 +0200

    feat(dataMessage): parse message infos

--- src/dataMessages.js
+++ src/dataMessages.js
@@ -2,13 +2,20 @@ var { default: srtParser2 } = require('srt-parser-2')
 const fs = require('fs');
 const { convertTimeToMilis } = require('./helpers/convertTimeToMilis');
 
-const changeTimeData = (data) => {
+const changeData = (data) => {
   let lastTime = 0
   data.map((d) => {
+    // parse time
     d.startTimeMilis = convertTimeToMilis(d.startTime)
     d.endTimeMilis = convertTimeToMilis(d.endTime)
     d.delayLastMilis = d.startTimeMilis  - lastTime
     lastTime = d.startTimeMilis
+
+    // parse message
+    const textSplit = d.text.split(/: (.+)?/, 2)
+    d.user = textSplit[0].trim()
+    d.message = textSplit[1].trim()
+    d.isWriting = textSplit[1].includes('[writing]')
 
     return d
   })
@@ -20,7 +27,9 @@ var parser = new srtParser2()
 const contentIntro = fs.readFileSync('./src/assets/Intro.srt').toString();
 const contentOutro = fs.readFileSync('./src/assets/Intro.srt').toString();
 
-var dataIntro = changeTimeData(parser.fromSrt(contentIntro));
-var dataOutro = changeTimeData(parser.fromSrt(contentOutro));
+var dataIntro = changeData(parser.fromSrt(contentIntro));
+var dataOutro = changeData(parser.fromSrt(contentOutro));
+
+console.log(dataIntro)
 
 module.exports = { dataIntro, dataOutro }
