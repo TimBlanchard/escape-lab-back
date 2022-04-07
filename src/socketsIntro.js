e646dbe0dcd5a545fe0e7e18255e25435e6a4c6c
valentinsld <valentin.salaud@gmail.com>
2022-04-07 10:01:14 +0200

    feat(socketIntro): send messages

--- src/socketsIntro.js
+++ src/socketsIntro.js
@@ -1,5 +1,36 @@
-const initSocketsIntro = (io, socket) => {
+const { dataIntro } = require('./dataMessages')
+const { rooms } = require('./roomServer')
 
+
+const userReadyIntro = (idRoom, socketID) => {
+  const existingRoom = rooms.find(roomObj => roomObj.id.trim().toLowerCase() === idRoom?.trim()?.toLowerCase())
+
+  if (!existingRoom) return { error: 'No Room'}
+
+  return existingRoom.introReady(socketID)
+}
+
+const initSocketsIntro = (io, socket) => {
+  socket.on('intro-recevedMessage', () => {
+    const data = userReadyIntro(socket.idRoom, socket.id)
+
+    // launch
+    if (data.canSendNextMessage && dataIntro.length > data.indexMessage) {
+      const infoMessage = dataIntro[data.indexMessage]
+      setTimeout(() => {
+        io.to(socket.idRoom).emit('intro-message', infoMessage)
+      }, infoMessage.delayLastMilis);
+    } else if (dataIntro.length <= data.indexMessage) {
+      const lastMessage = dataIntro[data.indexMessage - 1]
+      setTimeout(() => {
+        io.to(socket.idRoom).emit('intro-startVideo')
+      },  lastMessage.endTimeMilis - lastMessage.startTimeMilis);
+    }
+  })
+
+  socket.on('intro-endVideo', () => {
+    io.to(socket.idRoom).emit('setStepGame', { stepGame: 'Enigme1' })
+  })
 }
 
 module.exports = { initSocketsIntro }
