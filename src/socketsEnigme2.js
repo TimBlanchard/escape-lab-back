3190f5201d6424773aa465dabba9f32fded66232
Robin Verona <robin.verona@edu.gobelins.fr>
2022-04-07 19:44:50 +0200

    feat(enigme2): add base handling for popup across screens

--- src/socketsEnigme2.js
+++ src/socketsEnigme2.js
@@ -1,5 +1,47 @@
 // eslint-disable-next-line no-unused-vars, unused-imports/no-unused-vars
 const initSocketsEnigme2 = (io, socket) => {
+    const dataPopups = [
+        {
+            id: 1, 
+            text: `popup n°${this.id}`
+        },
+        {
+            id: 2, 
+            text: `popup n°${this.id}`
+        },
+        {
+            id: 3, 
+            text: `popup n°${this.id}`
+        },
+        {
+            id: 4, 
+            text: `popup n°${this.id}`
+        },
+        {
+            id: 5, 
+            text: `popup n°${this.id}`
+        },
+        {
+            id: 6, 
+            text: `popup n°${this.id}`
+        }
+    ]
+
+    socket.on('sendPopups', () => {
+        io.to(socket.idRoom).emit('sendPopups', dataPopups)
+        // console.log('sendPopups', socket)
+    })
+
+    socket.on('popupIsReady', () => {
+        console.log('popupIsReady');
+        io.to(socket.idRoom).emit('sendPopupToPlayer')
+        
+    })
+
+    socket.on('p2pPopup', () => {
+        io.to(socket.idRoom).emit('popupTransfer')
+        
+    })
 
 }
 
