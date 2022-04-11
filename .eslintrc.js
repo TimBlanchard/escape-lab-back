abe10600357f6cce26b2c8198428412e7e7b97b9
valentinsld <valentin.salaud@gmail.com>
2022-04-11 14:12:24 +0200

    ci(eslint): add airbnb-base

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,27 +2,30 @@ module.exports = {
   root: true,
   env: {
     node: true,
-    es6: true
+    es6: true,
   },
-  extends: ['eslint:recommended'],
+  extends: ['eslint:recommended', 'airbnb-base'],
   plugins: ['unused-imports', 'simple-import-sort', 'eslint-plugin-prettier'],
   parserOptions: {
     parser: 'babel-eslint',
     sourceType: 'module',
-    ecmaVersion: 11
+    ecmaVersion: 11,
   },
   rules: {
     quotes: ['error', 'single'],
     'unused-imports/no-unused-imports': 'error',
     'unused-imports/no-unused-vars': [
       'warn',
-      { vars: 'all', varsIgnorePattern: '^_', args: 'after-used', argsIgnorePattern: '^_' }
+      {
+        vars: 'all', varsIgnorePattern: '^_', args: 'after-used', argsIgnorePattern: '^_',
+      },
     ],
     'simple-import-sort/imports': 'error',
     'simple-import-sort/exports': 'error',
     'no-loss-of-precision': 'off',
     'no-nonoctal-decimal-escape': 'off',
     'no-unsafe-optional-chaining': 'off',
-    'no-useless-backreference': 'off'
-  }
+    'no-useless-backreference': 'off',
+    semi: 'off',
+  },
 }
