6db0088424de357944043c08b375fe58308ddd27
valentinsld <valentin.salaud@gmail.com>
2022-04-11 14:13:03 +0200

    refactor(helpers): fix lint

--- src/helpers/generateUID.js
+++ src/helpers/generateUID.js
@@ -2,12 +2,12 @@
 // Generate UID
 //
 function generateUID() {
-  // I generate the UID from two parts here 
+  // I generate the UID from two parts here
   // to ensure the random number provide enough bits.
-  var firstPart = (Math.random() * 46656) | 0;
-  var secondPart = (Math.random() * 46656) | 0;
-  firstPart = ('000' + firstPart.toString(36)).slice(-3);
-  secondPart = ('000' + secondPart.toString(36)).slice(-3);
+  let firstPart = Math.round(Math.random() * 46656);
+  let secondPart = Math.round(Math.random() * 46656);
+  firstPart = (`000${firstPart.toString(36)}`).slice(-3);
+  secondPart = (`000${secondPart.toString(36)}`).slice(-3);
   return firstPart + secondPart;
 }
 
