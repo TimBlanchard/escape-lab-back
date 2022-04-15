53cfd6be1a68d15f263cad89f683796f712e32da
TimBlanchard <timothee.blanchard37@gmail.com>
2022-04-15 10:54:36 +0200

    refactor(enigme-3): add randomNum helper and update code

--- src/socketsEnigme3.js
+++ src/socketsEnigme3.js
@@ -1,6 +1,7 @@
 // eslint-disable-next-line no-unused-vars, unused-imports/no-unused-vars
 
 const { enigme3Data, pricesData } = require('./data/enigme3')
+const { randomNum } = require('./helpers/randomNum')
 
 function generateConfig() {
   // CHOOSE SELLER WILL BE BOT OR NOT
@@ -11,7 +12,7 @@ function generateConfig() {
   const trueRules = enigme3Data().rules.sort(() => Math.random() - Math.random()).slice(0, trueRulesNumber)
 
   // CHOOSE RANDOM PRODUCT
-  const product = enigme3Data().products[Math.floor(Math.random() * enigme3Data().products.length)]
+  const product = enigme3Data().products[randomNum(0, enigme3Data().products.length)]
 
   // check if has normal or bot image
   const image = trueRules.some((obj) => obj.slug === 'stock') ? product.botImg : product.normalImg
@@ -22,19 +23,19 @@ function generateConfig() {
   let subtypeInterval
   if (product.type === 'clothing') {
     // select random value in marque array
-    const val = subtype.value[Math.floor(Math.random() * subtype.value.length)]
+    const val = subtype.value[randomNum(0, subtype.value.length)]
     subtypeText = subtype.name + val
     subtypeInterval = pricesData[product.type][val]
   } else {
     // choose randomly if will be value more or less
     if(Math.random() < 0.5) {
       // more than val
-      const val = subtype.value + Math.floor(Math.random() * subtype.value * 0.4)
+      const val = subtype.value + randomNum(1, subtype.value * 0.4)
       subtypeText = subtype.name + val.toString() + subtype.unit
       subtypeInterval = pricesData[product.type].more
     } else {
       // less than value
-      const val = subtype.value - Math.floor(Math.random() * subtype.value * 0.4)
+      const val = subtype.value - randomNum(1, subtype.value * 0.4)
       subtypeText = subtype.name + val.toString() + subtype.unit
       subtypeInterval = pricesData[product.type].less
     }
