b9329535ec463fa7fb2c351fa3c11b1041230c51
valentinsld <valentin.salaud@gmail.com>
2022-05-03 21:37:54 +0200

    feat(socketsEnigme1): add event on enigme1-recall

--- src/socketsEnigme1.js
+++ src/socketsEnigme1.js
@@ -1,6 +1,9 @@
 // eslint-disable-next-line no-unused-vars, unused-imports/no-unused-vars
 const initSocketsEnigme1 = (io, socket) => {
-
+  // on recall
+  socket.on('enigme1-recall', () => {
+    io.to(socket.idRoom).emit('enigme1-recall')
+  })
 }
 
 module.exports = { initSocketsEnigme1 }
