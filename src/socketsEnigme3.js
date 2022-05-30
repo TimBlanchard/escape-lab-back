b761d6fe60f91382a3db0af6b560197b78e01c6f
valentinsld <valentin.salaud@gmail.com>
2022-05-30 14:08:08 +0200

    refactor(socketsEnigme3): remove log

--- src/socketsEnigme3.js
+++ src/socketsEnigme3.js
@@ -3,7 +3,6 @@ const { setConfigEnigme3 } = require('./roomServer')
 const initSocketsEnigme3 = (io, socket) => {
   socket.on('enigme3-config', () => {
     const config = setConfigEnigme3(socket.idRoom)
-    console.log(config, socket.idRoom)
     io.to(socket.idRoom).emit('enigme3-config', config.config)
   })
 }
